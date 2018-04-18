c75775079d5ad70aa25807c54de597a2d63ebc70
Thomas Grainger <tagrain@gmail.com>
2018-04-18 09:28:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  rules: {
+    'mocha/handle-done-callback': 'error',
+    'mocha/no-global-tests': 'error',
+    'mocha/no-identical-title': 'error',
+    'mocha/no-return-and-callback': 'error',
+    'mocha/no-top-level-hooks': 'error',
+    'mocha/no-skipped-tests': 'error',
+    'mocha/no-mocha-arrows': 'error',
+    'mocha/max-top-level-suites': 'error',
+    'mocha/valid-test-description': 'error',
+    'mocha/no-nested-tests': 'error',
+    'mocha/no-synchronous-tests': 'error',
+    'mocha/no-sibling-hooks': 'error',
+    'mocha/no-hooks': 'error',
+    'mocha/no-exclusive-tests': 'error',
+    'mocha/no-hooks-for-single-case': 'error',
+    'mocha/no-pending-tests': 'error',
+    'mocha/valid-suite-description': 'error'
+  },
+};
